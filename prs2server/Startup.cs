bfeec4c575303de73a02841c0df9142da5eb17dd
Greg Doud <gpdoud@gmail.com>
2020-08-06 10:52:23 -0400

    Added connection for hosting on Winhost

--- prs2server/Startup.cs
+++ prs2server/Startup.cs
@@ -29,9 +29,13 @@ namespace prs2server {
             services.AddDbContext<Prs2DbContext>(option => {
                 option.UseLazyLoadingProxies();
                 option.UseSqlServer(Configuration.GetConnectionString("Prs2DbContext"));
+                //option.UseSqlServer(Configuration.GetConnectionString("Winhost"));
             });
 
             services.AddCors();
+
+            //var secret = Configuration["Application:codeword"];
+            //Configuration["Test"];
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,8 +54,11 @@ namespace prs2server {
                 endpoints.MapControllers();
             });
 
-            using var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
-            scope.ServiceProvider.GetService<Prs2DbContext>().Database.Migrate();
+            using var scope = app.ApplicationServices
+                                    .GetRequiredService<IServiceScopeFactory>()
+                                    .CreateScope();
+            scope.ServiceProvider.GetService<Prs2DbContext>()
+                                    .Database.Migrate();
             
 
         }
